e4c572730f43ecf254081c82578f9c510d4fcee9
Valentyna Dombrovska <99506124+vdombrovska@users.noreply.github.com>
2022-05-04 22:31:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const contactForm = document.querySelector('#newForm');
+const contactsList = document.querySelector('#contactsList');
+const contactTemplate = document.querySelector('#userTemplate');
+const DELETE_Cl = 'delete';
+contactForm.addEventListener('click', onAddBtnClick);
+contactsList.addEventListener('click', onDeleteBtnClick);
+
+
+function onAddBtnClick(line) {
+    line.preventDefault();
+    const newContact = getContact();
+    addNewLine(newContact);
+    clearInputs();
+}
+
+function onDeleteBtnClick (line) {
+    if (line.target.classList.contains(DELETE_CL)) {
+        line.remove;
+}}
+
+function getContact() {
+    const contactFields = {};
+    contactFields.firstName = document.getElementById ('inputFirstName');
+    contactFields.lastName = document.getElementById ('inputLAstName');
+    contactFields.phoneNumber = document.getElementById ('InputUsersTel');
+    return contactFields;
+}
+
+function areFieldsCorrect(contactFields) {
+    return (
+        isFirstNameCorrect(contactFields.firstName) &&
+        isLastNameCorrect(contactFields.lastName) &&
+        isPhoneCorrect(contactFields.phoneNumber)
+    );
+}
+
+function isFirstNameCorrect (value) {
+    if (value =='')
+    return error();
+}
+
+function isLastNameCorrect (value) {
+    if (value =='')
+    return error();
+}
+
+function isPhoneCorrect (value) {
+    if( value =='' || isNaN(value))
+    return error();
+}
+
+function error(){
+    return alert (' error')
+}
+
+function clearInputs (){
+    contactFields.firstName = '';
+    contactFields.lastName = '';
+    contactFields.phoneNumber = '';
+}
+
+function addNewLine (contactFields){
+    const newLineHtml = replaceFunction(contactFields); 
+    contactsList.insertAdjacentHTML ('beforeend',newLineHtml)
+}
+
+function replaceFunction (contactFields){
+    return
+    contactTemplate.replace( {firstName}, contactFields.firstName);
+    contactTemplate.replace( {lastName}, contactFields.lastName);
+    contactTemplate.replace( {phone}, contactFields.phoneNumber)}
